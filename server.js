2fd745e276167e4391a1319d9c9b13ff1dcf41a6
Montyyy <danyramadhan558@gmail.com>
2026-04-25 20:55:09 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 mongoose.connect('mongodb://localhost:27017/kampus_db')
     .then(() => console.log('Terhubung ke database kampus!'))
-    .catch((err) => console.log('Gagal terhubung:', err));
+    .catch((err) => console.log('Gagal terhubung !:', err));
     const prodiSchema = new mongoose.Schema({
     nama_prodi: String,
     fakultas: String,
